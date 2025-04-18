24675e7468e26fbc601f61d0a0db57c13fbf3513
mimibedeli <mmelend2@terpmail.umd.edu>
2025-04-18 17:51:00 -0400

    updating the shuffle function again

--- word_shuffle.py
+++ word_shuffle.py
@@ -11,61 +11,30 @@ def play_word_game(n=8):
 
     Returns:
         dict: A dictionary with the player's guess, the letters shown, and 
-    whether the guess was valid.
+        whether the guess was valid.
     """
-<<<<<<< HEAD
-    
     try:
-        with open(word_file, 'r') as file:
-            valid_words = set(word.strip().lower() for word in file 
-                              if word.strip())
-=======
-    try:
-        with open("words.txt" , 'r') as file:
+        with open("words.txt", 'r') as file:
             valid_words = set(word.strip().lower() for word in file if 
                               word.strip())
->>>>>>> 1446c1c (updating shuffle function)
     except FileNotFoundError:
-        print(f"File 'words.txt' not found.")
-        return {'valid_guess': False, 'random_letters': [], 'guess': guess}
+        print("File 'words.txt' not found.")
+        return {'valid_guess': False, 'random_letters': [], 'guess': None}
 
-<<<<<<< HEAD
-    
-    letters = list("eeeeaaaaiiiooouuulnstrcmphbdg")
-    random_letters = random.sample(letters, n)
-
-    
-=======
     letters = list("eeeeaaaaiiiooouuulnstrcmphbdg")
     random_letters = random.sample(letters, n)
 
     print("Your letters are:", " ".join(random_letters))
     guess = input("Enter your word guess: ").lower().strip()
 
->>>>>>> 1446c1c (updating shuffle function)
-    guess = guess.lower()
     guess_count = {}
     for char in guess:
         guess_count[char] = guess_count.get(char, 0) + 1
 
-<<<<<<< HEAD
-=======
->>>>>>> 1446c1c (updating shuffle function)
     available_count = {}
     for char in random_letters:
         available_count[char] = available_count.get(char, 0) + 1
 
-<<<<<<< HEAD
-    
-    if guess not in valid_words:
-        return {'valid_guess': False, 'random_letters': random_letters,
-                 'guess': guess}
-
-    for letter in guess_count:
-        if guess_count[letter] > available_count.get(letter, 0):
-            return {'valid_guess': False, 'random_letters': random_letters,
-                     'guess': guess}
-=======
     if guess not in valid_words:
         return {'valid_guess': False, 'random_letters': random_letters, 
                 'guess': guess}
@@ -74,10 +43,10 @@ def play_word_game(n=8):
         if guess_count[letter] > available_count.get(letter, 0):
             return {'valid_guess': False, 'random_letters': random_letters, 
                     'guess': guess}
->>>>>>> 1446c1c (updating shuffle function)
 
-    return {'valid_guess': True, 'random_letters': random_letters, 'guess': 
-        guess}
+    return {'valid_guess': True, 'random_letters': random_letters, 
+            'guess': guess}
+
 
 if __name__ == "__main__":
     result = play_word_game()
