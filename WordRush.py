053b1866a1070d1b7c188c066c51c2998acdae06
mimibedeli <mmelend2@terpmail.umd.edu>
2025-05-06 15:53:08 -0400

    removing the word_formation.py file and setting up the basics for the first wildcard function.

--- WordRush.py
+++ WordRush.py
@@ -1,5 +1,9 @@
 import random
 
+
+def game_introduction():
+    print("Welcome to WordRush!")
+
 def play_game():
     try:
         with open("words.txt", 'r') as file:
@@ -8,10 +12,16 @@ def play_game():
         print("File 'words.txt' not found.")
         return
 
-    letters = list("eeeeaaaaiiiooouuulnstrcmphbdg")
+    letters = list("abcdefghijklmnopqrstuvwxyz")
     random_letters = random.sample(letters, 8)
-    print("Use these letters to form valid words (type 'quit' to exit):")
-    print("Your letters:", " ".join(random_letters))
+    print("\nUse these letters to form valid words (type 'quit' to exit):")
+    print("Your letters: ", " ".join(random_letters))
+    print()
+    
+    use_powerup = input("Do you want to use the Wildcard Power-Up? (yes/no): "
+                        ).strip().lower()
+    if use_powerup == "yes":
+        random_letters = wildcard_powerup(random_letters)
 
     available_count = {}
     for char in random_letters:
@@ -40,6 +50,35 @@ def play_game():
             print(" Valid guess!")
         else:
             print("Invalid use of letters.")
+            
+            
+def wildcard_powerup(letters):
+    print("[WILDCARD ACTIVATED!] You can add or remove one letter!")
+    player_choice = (
+        input("Type 'add' to add a letter or 'remove' to remove one: ")
+        .strip().lower()
+    )   
+    
+    if player_choice == "add":
+        new_letter = input("Enter the letter you want to add: ").strip().lower()
+        letters.append(new_letter)
+        print(f"New letter set: {' '.join(letters)}")
+
+    elif player_choice == "remove":
+        remove_letter = (
+            input("Enter the letter you want to remove: ").strip().lower()
+        )
+        letters.remove(remove_letter)
+        print(f"New letter set: {' '.join(letters)}")
+
+    return letters
+                
+            
+            
+            
+
 
 if __name__ == "__main__":
     play_game()
+
+
