785233a17d425238c6dfd714ef8ad3fe77ac068e
mimibedeli <mmelend2@terpmail.umd.edu>
2025-05-13 17:25:26 -0400

    removing unneccessary files in the folder.

--- First_commit.py
+++
@@ -1,7 +0,0 @@
-print("Germid made this line")
-
-print ("Hung has made an edit to this file")
-
-print ("Michelle has made an edit to this file") 
-
-print ("Dulcinea has made an edit to this file")
